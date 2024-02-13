af0d76806a208b1c58ec6740d5afa8642a9b57fb
mouezelaifa <mouezelaifa@gmail.com>
2024-02-13 12:28:33 -0800

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python3
-"""Authentication module.
-"""
+"""module for the class Auth"""
+
 from flask import request
 from typing import List, TypeVar
-import fnmatch
+from fnmatch import fnmatch
 
 
 class Auth:
-    """Authentication class.
-    """
+    """A class handling authentication-related functionality."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
-        """
-        if path is None:
+        """Determines if authentication is required for the given path."""
+        if not path or not excluded_paths:
             return True
-
-        if excluded_paths is None or not excluded_paths:
-            return True
-
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
-
-        return True
+        if path[-1] != '/':
+            path += '/'
+        return not [n for n in excluded_paths if fnmatch(path, n)]
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """Retrieves the authorization header from the provided request."""
+        if not request:
+            return None
+        return request.headers.get('Authorization', None)
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request.
-        """
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Retrieves the current user based on the provided request."""
         return None
